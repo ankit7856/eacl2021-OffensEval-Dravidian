e93fe609041aeec107a12499cf38db0c8bedc09a
murali1996 <jsaimurali001@gmail.com>
2021-02-14 12:11:17 -0800

    commit for paper submission

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # EACL 2021 OffensEval in Dravidian Languages    
-### (Submission team SJ_AJ)
+### (Team SJ_AJ's submission files)
 
+### Leaderboard
+| Language  | Rank | Precision | Recall | F1   |
+|-----------|------|-----------|--------|------|
+| Kannada   | 1    | 0.73      | 0.78   | 0.75 |
+| Malayalam | 2    | 0.97      | 0.97   | 0.96 |
+| Tamil     | 3    | 0.75      | 0.79   | 0.76 |
+
+### Notes
 - Except for code, other things (like pretrained models, submission files and checkpoints) can be downloaded from this [drive repo](https://drive.google.com/drive/folders/1xnQ63uZ7Pq1go1K21OkgRkGFUkfizoT7?usp=sharing).
 - The scripts for creating these pretrained models are placed at [./pretraining](./pretraining)
 - See [./code/datasets/eacl2021](./code/datasets/eacl2021) for creating data and running code. However, the main scripts are located in [code/scripts](.code/scripts) folder.
 - The .ipynb files (colab files) use absolute paths. So be careful and please change them according to your local directory path(s).
-- Note: Several snippets of code were derived from Sai's Capstone Project
+- See [./code/datasets/eacl2021/run.sh](./code/datasets/eacl2021/run.sh) for how to run the code. Alternatively, see notebooks in [./colab_notebooks](./colab_notebooks)
+
+### Contact
+Feel free to contact us for a quick chat or a discussion at [gmail](jsaimurali001@gmail.com)
--- code/datasets/eacl2021/run.sh
+++ code/datasets/eacl2021/run.sh
@@ -2,6 +2,18 @@
 
 #rm -r ../../checkpoints/eacl2021
 #cd ../../scripts
+#CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --max-epochs 10 --mode train_dev_test --model-name bert-charlstm-lstm --text-type "trt" --dataset-name eacl2021/offeval/kannada
+#CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --max-epochs 10 --mode train_dev_test --model-name bert-charlstm-lstm --text-type "trt" --dataset-name eacl2021/offeval/tamil
+#CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --max-epochs 10 --mode train_dev_test --model-name bert-charlstm-lstm --text-type "trt" --dataset-name eacl2021/offeval/malayalam
+
+#rm -r ../../checkpoints/eacl2021
+#cd ../../scripts
+#CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --max-epochs 10 --mode train_dev_test --model-name bert-charlstm-lstm --text-type "" --dataset-name eacl2021/offeval/kannada
+#CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --max-epochs 10 --mode train_dev_test --model-name bert-charlstm-lstm --text-type "" --dataset-name eacl2021/offeval/tamil
+#CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --max-epochs 10 --mode train_dev_test --model-name bert-charlstm-lstm --text-type "" --dataset-name eacl2021/offeval/malayalam
+
+#rm -r ../../checkpoints/eacl2021
+#cd ../../scripts
 #CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --mode train_dev --model-name xlm-roberta-base --text-type "" --dataset-name eacl2021/offeval/combined
 #CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --mode dev_kannada --model-name xlm-roberta-base --text-type "" --dataset-name eacl2021/offeval/combined --eval-ckpt-path ../checkpoints/eacl2021/offeval/combined/xlm-roberta-base/text_raw/
 #CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --mode dev_tamil --model-name xlm-roberta-base --text-type "" --dataset-name eacl2021/offeval/combined --eval-ckpt-path ../checkpoints/eacl2021/offeval/combined/xlm-roberta-base/text_raw/
@@ -17,7 +29,6 @@
 #rm -r ../checkpoints/eacl2021
 #CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --mode train_dev --model-name bert-charlstm-lstm --text-type "" --dataset-name eacl2021/offeval/malayalam --max-epochs 10 --custom-pretrained-path ../../pretraining/offeval/malayalam/xlm-roberta-base/
 #CUDA_VISIBLE_DEVICES=0 python run_eacl2021.py --mode train_dev --model-name bert-fasttext-lstm --text-type "trt" --dataset-name eacl2021/offeval/malayalam --max-epochs 10 --custom-pretrained-path ../../pretraining/offeval/malayalam/xlm-roberta-base/
-
 
 #rm -r ../../checkpoints/eacl2021
 #cd ../../scripts
